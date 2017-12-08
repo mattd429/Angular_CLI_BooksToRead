e8455cc0f0ee5743d960660de989e54082bb5347
Matthew Brown <mattd429@gmail.com>
2017-12-08 16:21:44 -0500

    Update books.components.ts
    what we do here is import Components, OnInIt, books typescript from app module and our mock data of books. add our component so we can share out our selector for HTML and css.  then create our class by class BookComponent and create a method for when a book is selected, which assigns the clicked book from the template to the component's `selectedBook`, then we add another component for our mock data for books, last we add constructor and ngOnInIt()

--- src/app/books/books.components.ts
+++ src/app/books/books.components.ts
@@ -1,1 +1,24 @@
+import { Component, OnInIt } from '@angular/core';
+import { Book } from '../book';
+import {BOOKS} from '../mock-books';
 
+@Component({
+  selector: 'app-books',
+  templateUrls: './books.components.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInIt {
+  selectedBook: Book;
+  
+  onSelected(book: Book): void {
+    this.selectedBook = book;
+  }
+  
+  books = BOOKS;
+  
+  constructor() { }
+  
+  ngOnInIt() {
+  }
+
+}
