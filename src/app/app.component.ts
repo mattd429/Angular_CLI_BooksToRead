5766f4f5fac92284ce09d374893e245d5e3c0e20
Matthew Brown <mattd429@gmail.com>
2017-12-08 13:45:32 -0500

    Update app.component.ts
    creating our components of our main app section, as you can see we added a title calling it Books to Read. our selector(we wont have to worried about that) for adding into the main html, which is allowing us to import our styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,1 +1,10 @@
+import { Component } from 'angular/core';
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  setyleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Books to Read';
+}
