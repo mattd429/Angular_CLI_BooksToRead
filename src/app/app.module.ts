32b02ee7eb1af286b7220dcdd732e7ecdb223fcf
Matthew Brown <mattd429@gmail.com>
2017-12-08 15:12:59 -0500

    Update app.module.ts
    Defines AppModule, the root module that tells angular with an HTML template, CSS stylesheet, and a unit test.  it is the root component of what will become a tree nested components as the application evolves.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,23 @@
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModule lives here.
+import { NgModule } from '@angular/core';
 
+import { AppComponent } from './app.component';
+import { BookComponents } from './books/books.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BooksComponent
+  ],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
+    FormsModule
+  ],
+  providers: [],
+  boostrap: [AppComponent]
+})
+export class AppModule { }
