04263f07677a98637a6ba2cf2c93ee6c7233cd90
Matthew Brown <mattd429@gmail.com>
2017-12-08 20:01:30 -0500

    Update mock-books.ts
    creating a mock file for now until we are ready to use a real DB.

--- src/app/mock-books.ts
+++ src/app/mock-books.ts
@@ -1,1 +1,14 @@
+import { Book } from './book';
 
+export const BOOKS: book[] = [
+  { id: 11, name: 'Secret Societies of Americas Elite' },
+  { id: 12, name: 'ALERT' },
+  { id: 13, name: 'The Mad and The Bad' },
+  { id: 14, name: 'The Mystical Qabalah' },
+  { id: 15, name: 'Back Roads Trilogy' },
+  { id: 16, name: 'Bruce Lee: Artist of Life'},
+  { id: 17, name: 'Nothing to Envy' },
+  { id: 18, name: 'The Road to Character' },
+  { id: 19, name: 'Open Secrets' },
+  { id: 20, name: 'The Grass Grows by Itself' }
+];
