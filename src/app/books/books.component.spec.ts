f729b80aa9de7800899fc8e59131d860356afa50
Matthew Brown <mattd429@gmail.com>
2017-12-08 19:28:51 -0500

    Update books.component.spec.ts
    for testing our Book component.

--- src/app/books/books.component.spec.ts
+++ src/app/books/books.component.spec.ts
@@ -1,1 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookComponent } from './books.component';
+
+// Fixture for debugging and testing a component...
+describe('BookComponents', () => {
+  let component: BooksComponent;
+  let fixture: ComponentFixture<BooksComponent>;
+  
+  // Configures and initializes environment for unit testing and provides methods for creating components and services in unit tests.
+  beforeEach(async()) => {
+    TestBed.configureTestingModule({
+      declarations: [ BooksComponent ]
+    })
+    .compileComponent();
+  }));
+  
+    beforeEach(() => {
+    fixture = TestBed.createComponent(BooksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
 
