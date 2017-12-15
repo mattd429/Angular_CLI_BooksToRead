aa88f74bad9f570a66cf8b0a16348fb4b797c725
Matthew Brown <mattd429@gmail.com>
2017-12-15 10:21:01 -0500

    Update book-detail.component.ts
    we have here out component.ts which is sub component of BookComponent. basically the parent is BookComponent and the BookDetailComponent is the child. we have added our Input so that BookComponent can bind to this Component.

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -1,1 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../book';
 
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+  //BooksComponent will bind to this...
+  @Input() book: Book;
+  
+  constructor() { }
+  
+  ngOnInit() {
+  
+  }
+}
