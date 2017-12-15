93a71315cb8ba0c25a803ca1ced16923f61e82aa
Matthew Brown <mattd429@gmail.com>
2017-12-15 14:24:15 -0500

    Update book-detail.component.spec.ts
    What we have here is a spec for unit testing, we want to test BookDetailComponent to make sure to test the components in our book-detail.component.ts. this will allow it to a browser ready karma connected to show for passes or fails.

--- src/app/book-detail/book-detail.component.spec.ts
+++ src/app/book-detail/book-detail.component.spec.ts
@@ -1,1 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { BookDetailComponent } from './book-detail.component';
+
+describe('BookDetailComponent', () => {
+  let component: BookDetailComponet;
+  let fixture: ComponentFixture<BookDetailComponent>;
+  
+  beforeEach(async()) => {
+    TestBed.configureTestingModule({
+      // used to declare components is this module
+      declarations: [ BookDetailComponent ]
+    })
+    .compileComonents();
+  }));
+  
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detechChanges(); 
+  });
+  
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
