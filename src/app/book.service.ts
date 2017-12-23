d39c343e0cded2e863bb2fda67bcaad79d4fdef8
Matthew Brown <mattd429@gmail.com>
2017-12-23 11:20:50 -0500

    Update book.service.ts
    import observable to call our data in async() instead of sync.  adding of() to simulate getting data from the server.

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 import { Book } from './book';
 import { BOOKS } from './mock-books';
