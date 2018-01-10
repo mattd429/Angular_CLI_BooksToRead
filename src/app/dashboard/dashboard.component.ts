b8cde4bb7a02229f615778d273a9be1d67bd790e
Matthew Brown <mattd429@gmail.com>
2018-01-10 18:54:22 -0500

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../book';
 import { BookService } from '../book.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard/component.html',
+  styleUrls: ['./dashboard.component.css']
+})
